91e23655f30f34e8b630e667ba20b359024a67ef
madgamesmith <madgamesmith@gmail.com>
2021-04-28 07:41:19 +0200

    Bot init

---
+++ index.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const client = new Discord.Client(); 
+client.login('ODM2NjU5NDgwNjk5NzMxOTY4.YIhN3A.TWxtIUB-xXojoeRvS5a05a88Z0s');
+
+const instructionsChannel = '<#836688382804885544>';
+
+client.on('ready', readyDiscord); //On login auth. successfull
+function readyDiscord(){
+    console.log('good stuff');
+}
+
+client.on('message', messageSentByUser);
+function messageSentByUser(msg){
+    if(msg.channel.id == '790858643393609780' && msg.content == "test"){
+        msg.channel.send(`Welcome to the server! Please read the ` + instructionsChannel);
+        msg.channel.send(`:wave:`);
+    }
+}
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.cache.find(ch => ch.name === 'introductions');
+    if (!channel) return;
+    channel.send(`Welcome to the server ${member}`);
+    channel.send(`:wave:`);
+    channel.send(`Please read the ` + instructionsChannel);
+});
+
+  
