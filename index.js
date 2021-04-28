f073fc173aaf87fee7418244113c0cd5ded81c22
madgamesmith <madgamesmith@gmail.com>
2021-04-28 21:11:06 +0200

    ping pong check added

--- index.js
+++ index.js
@@ -14,12 +14,11 @@ function readyDiscord(){
     console.log('good stuff');
 }
 
-// client.on('message', msg => {
-//     if(msg.channel.id == '790858643393609780' && msg.content == "test"){
-//         msg.channel.send(`Welcome to the server! Please read the ` + rulesChannel);
-//         msg.channel.send(`:wave:`);
-//     }
-// });
+client.on('message', msg => {
+    if(msg.content == "!ping"){
+        msg.react('🏓');
+    }
+});
 
 client.on('guildMemberAdd', member => {
     const channel = member.guild.channels.cache.find(ch => ch.name === 'introductions');
