61b55aca9a9410c7b2ee53779c6325b923a042d5
madgamesmith <madgamesmith@gmail.com>
2021-04-28 20:55:56 +0200

    ready to work

--- index.js
+++ index.js
@@ -7,26 +7,26 @@ const client = new Discord.Client({
 const botToken = process.env.BOT_TOKEN;
 client.login(botToken);
 
-const instructionsChannel = '<#836688382804885544>';
+const rulesChannel = '<#836688382804885544>';
 
 client.on('ready', readyDiscord); //On login auth. successfull
 function readyDiscord(){
     console.log('good stuff');
 }
 
-client.on('message', msg => {
-    if(msg.channel.id == '790858643393609780' && msg.content == "test"){
-        msg.channel.send(`Welcome to the server! Please read the ` + instructionsChannel);
-        msg.channel.send(`:wave:`);
-    }
-});
+// client.on('message', msg => {
+//     if(msg.channel.id == '790858643393609780' && msg.content == "test"){
+//         msg.channel.send(`Welcome to the server! Please read the ` + rulesChannel);
+//         msg.channel.send(`:wave:`);
+//     }
+// });
 
 client.on('guildMemberAdd', member => {
     const channel = member.guild.channels.cache.find(ch => ch.name === 'introductions');
     if (!channel) return;
     channel.send(`Welcome to the server ${member}`);
     channel.send(`:wave:`);
-    channel.send(`Please read the ` + instructionsChannel);
+    channel.send(`Please read the ` + rulesChannel);
 });
 
   
