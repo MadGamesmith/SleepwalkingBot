66005c2edd3dd8f2e26614e182517bf0ed59ab8f
Glitch (rainy-showy-log) <none>
2021-04-28 06:08:25 +0000

    test

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client(); 
-<<<<<<< HEAD
-client.login('ODM2NjU5NDgwNjk5NzMxOTY4.YIhN3A.TWxtIUB-xXojoeRvS5a05a88Z0s');
-=======
-const botLogin = process.env.BOT_LOGIN;
-client.login(botLogin);
->>>>>>> 64c538009ed1e797924b13a40bcced89446b1ead
+client.login('?');
 
 const instructionsChannel = '<#836688382804885544>';
 
